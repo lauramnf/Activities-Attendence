bf169ba8f4a4e542c2aab46316f8c8bd2e920fde
Laura Maciel <laura.neves2008@hotmail.com>
2020-10-19 17:23:32 -0300

    Wrong code, see what to fix

--- presenca_teams.py
+++ presenca_teams.py
@@ -13,8 +13,12 @@ def conta_frequencia_atividades(tipo, semana, pontos):
 		for row in csv_f:
 
 			nome = row['Nome Completo'].lower()
-			nome = nome.strip(' (convidado)')
-			nome = nome.strip(' (guest)')
+			nome = nome.replace(' (convidado)', '')
+			nome = nome.replace(' (guest)', '')
+			
+			for name in presenca.keys():
+				if nome in name:
+					nome = name
 			
 			if nome == 'live:.b44abc4b05463b85':
 				nome = 'ana beatriz ferreira da silva'
@@ -26,26 +30,19 @@ def conta_frequencia_atividades(tipo, semana, pontos):
 				nome = 'davi silva'
 			elif nome == 'live:.cid.ec4be6c1f9167e62':
 				nome = 'sarah souza'
+			elif nome == 'live:.cid.bc6ce3929d590f6f':
+				nome = 'thalita barros'
 			# elif nome == 'live:.cid.32b110fabe43d0b0':
 				# nome = 'live:.cid.32b110fabe43d0b0'
 			# elif nome == 'live:.cid.6a8df8fb916cf458':
 				# nome = 'live:.cid.6a8df8fb916cf458'
-			elif nome == 'live:.cid.74e6e116ee7a641d'
-				nome = 'paula de fátima':
+			# elif nome == ''
+				# nome = 'paula de fátima':
 			elif nome == 'live:.cid.74e6e116ee7a641d':
 				nome = 'caroline da silva'
-			elif 'ana clara' in nome:
-				nome = 'ana clara uecker'
 			elif nome == 'angela oliveira':
-				nome = 'angela oliveira'
-			elif 'barbara jordana' in nome:
-				nome = 'bárbara jordana da silva'
-			elif 'débora ferreira' in nome:
-				nome = 'débora lígia ferreira da silva'
-			elif 'gabriela soares' in nome:
-				nome = 'gabriela soares fonseca andrade'
-			
-			presenca[nome] = presenca.get(nome.lower(), 0) + pontos		 	 
+				nome = 'ângela oliveira'
+			presenca[nome] = presenca.get(nome, 0) + pontos		 	 
 		file.close()
 	return(presenca)
 
@@ -57,7 +54,11 @@ def conta_presenca_final(semana):
 		file = open(pasta+'/'+nome)
 		csv_f = csv.DictReader(file)
 		for row in csv_f:
-			presenca[row['Nome']] = presenca.get(row['Nome'], 0) + int(row['Pontos'])		 	 
+			nome = row['Nome']
+			for name in presenca.keys():
+				if nome in name:
+					nome = name
+			presenca[nome] = presenca.get(nome, 0) + int(row['Pontos'])		 	 
 		file.close()
 	return(presenca)
 
